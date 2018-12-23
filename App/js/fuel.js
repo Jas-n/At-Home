163b3920a0fa1fd8327c74d915b3280d9b95cb4a
Jason Millis <jas-n@outlook.com>
2018-12-23 20:12:15 +0000

    1.4.0

--- App/js/fuel.js
+++ App/js/fuel.js
@@ -3,49 +3,79 @@ var fuel={
 	name:		'Fuel Log',
 	init:function(){
 		this.render();
+		$('#fuel-date').val(php.date('Y-m-d'));
 		if(!this.initiated){
 			this.watch_new_fuel();
 			this.initiated=true;
 		}
 	},
 	render:function(){
-		//this.get_fuel();
+		this.get_fuel();
 	},
 	get_fuel:function(){
-		home.ajax('calendar','get_fuel',this.got_fuel);
+		home.ajax('fuel','get_fuel',this.got_fuel,{user:home.user});
 	},
 	got_fuel:function(json){
 		if(json.status){
-			var data=json.data;
-			var event={};
+			var logs	=json.data.logs;
+			var totals	=json.data.totals;
+			var log={};
 			$('.fuel').children().remove();
-			if(data.count){ 
-				console.log(data);
+			$('.fuel').append('<div class="card">'+
+				'<h3>Totals</h3>'+
+				'<div class="row">'+
+					'<div class="col-6">Mileage:</div>'+
+					'<div class="col-6">'+php.number_format(totals.mileage,1)+'</div>'+
+					'<div class="col-6">Cost:</div>'+
+					'<div class="col-6">&pound;'+php.number_format(totals.cost,2)+'</div>'+
+					'<div class="col-6">Litres:</div>'+
+					'<div class="col-6">'+php.number_format(totals.litres,2)+'</div>'+
+				'</div>'+
+			'</div>');
+			for(var i=0;i<logs.length;i++){
+				log=logs[i];
+				$('.fuel').append('<div class="card">'+
+					'<h3>'+php.formatted_date(log.date)+'</h3>'+
+					'<div class="row">'+
+						'<div class="col-6">Mileage:</div>'+
+						'<div class="col-6">'+log.mileage+'</div>'+
+						'<div class="col-6">Cost Per Litre:</div>'+
+						'<div class="col-6">&pound;'+php.number_format(log.per_litre,2)+'/Litre</div>'+
+						'<div class="col-6">Cost:</div>'+
+						'<div class="col-6">&pound;'+php.number_format(log.cost,2)+'</div>'+
+						'<div class="col-6">Litres:</div>'+
+						'<div class="col-6">'+log.litres+'</div>'+
+					'</div>'+
+				'</div>');
 			}
 		}
 	},
 	watch_new_fuel:function(){
 		$('main').on('click','.js-save-fuel',function(){
 			$('#modal-error').remove();
-			var name		=$('#event-name').val();
-			var type		=$('#event-type').val();
-			var start		=$('#event-date').val();
-			var recurrence	=$('#event-recurrence').val();
-			if(!name || !start){
-				$('.modal-body').prepend(bootstrap.alert('danger','An event must have a Name and Start Date',{id:'modal-error'}));
+			var date		=$('#fuel-date').val();
+			var mileage		=$('#fuel-mileage').val();
+			var per_litre	=$('#fuel-per-litre').val();
+			var cost		=$('#fuel-cost').val();
+			var litres		=$('#fuel-litres').val();
+			if(!date || !mileage || !per_litre || !cost || !litres){
+				$('.modal-body').prepend(bootstrap.alert('danger','Fuel must have a Date, Mileage, Cost Per Litre, Cost and Litres',{id:'modal-error'}));
 			}else{
-				home.ajax('calendar','add_event',function(){
-					$('#event_modal').modal('hide');
-					$('#event-name').val('');
-					$('#event-type').val('');
-					$('#event-date').val('');
-					$('#event-recurrence').val('');
-					calendar.get_schedule();
+				home.ajax('fuel','add_fuel',function(){
+					$('#fuel_modal').modal('hide');
+					$('#fuel-date').val(php.date('Y-m-d'));
+					$('#fuel-mileage').val('');
+					$('#fuel-per-litre').val('');
+					$('#fuel-cost').val('');
+					$('#fuel-litres').val('');
+					fuel.get_fuel(home.user);
 				},{
-					name		:name,
-					type		:type,
-					start		:start,
-					recurrence	:recurrence
+					date:		date,
+					mileage:	mileage,
+					per_litre:	per_litre,
+					cost:		cost,
+					litres:		litres,
+					user:		home.user
 				});
 			}
 		});
---
+++ Server/classes/fuel.php
@@ -0,0 +1,47 @@
+<?php class fuel{
+	function __construct(){
+	}
+	public function add_fuel($data){
+		global $db;
+		$db->query(
+			"INSERT INTO `fuel` (
+				`user`,	`date`,`mileage`,`per_litre`,`cost`,
+				`litres`
+			) VALUES (?,?,?,?,?,	?)",
+			array(
+				$data['user'],
+				$data['date'],
+				$data['mileage'],
+				$data['per_litre'],
+				$data['cost'],
+
+				$data['litres'],
+			)
+		);
+		return $this->get_fuel(array('user'=>$data['user']));
+	}
+	public function get_fuel($data=false){
+		global $db;
+		return array(
+			'logs'=>$db->query(
+				"SELECT *
+				FROM `fuel`
+				WHERE `user`=?
+				ORDER BY
+					`date` DESC,
+					`id` DESC
+				LIMIT ".ITEMS_PER_PAGE,
+				$data['user']
+			),
+			'totals'=>$db->get_row(
+				"SELECT
+					SUM(`mileage`) as `mileage`,
+					SUM(`cost`) as `cost`,
+					SUM(`litres`) as `litres`
+				FROM `fuel`
+				WHERE `user`=?",
+				$data['user']
+			)
+		);
+	}
+}
